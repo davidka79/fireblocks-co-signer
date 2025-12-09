2bf398e959a2bb4cd486007d2068ccc0e0807962
davidka79 <davidka79@gmail.com>
2025-12-09 19:11:55 +0300

    add vpc

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,8 @@
-# VPC 
+# VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = var.tags
 }
@@ -12,7 +13,7 @@ resource "aws_internet_gateway" "main" {
   tags   = var.tags
 }
 
-# Subnets 
+# Subnets
 
 # public for NAT Gateway
 resource "aws_subnet" "public" {
@@ -34,3 +35,54 @@ resource "aws_subnet" "private" {
   tags = var.tags
 }
 
+###############################################
+
+# public routing to GATEWAY
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = merge(var.tags, { Name = "${var.project_name}-public-rt" })
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# NAT gateway  EIP
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags   = merge(var.tags, { Name = "${var.project_name}-nat-eip" })
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = merge(var.tags, { Name = "${var.project_name}-nat" })
+}
+
+
+# private routing to NAT Gateway
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = merge(var.tags, { Name = "${var.project_name}-private-rt" })
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
